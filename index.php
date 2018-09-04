d05baa58a2f638f5127277a059552f7a586d7f7a
siart24 <bleart.lema@stud.kftg.ch>
2018-09-04 14:32:47 +0200

    MOTIVATION

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-echo "Zusammenkommen ist ein Beginn,";
-echo "zusammenbleiben ist ein Fortschritt,";
-echo "zusammenarbeiten ist ein Erfolg";
+echo "Zusammenkommen ist ein Beginn, <br>
+zusammenbleiben ist ein Fortschritt, <br>
+zusammenarbeiten ist ein Erfolg <br>";
+echo "- Henry Ford";
