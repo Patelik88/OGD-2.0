61b80637bc8adbc11969155fcb254fc5f66ae8c8
Pascal <pascal.leone@stud.kftg.ch>
2018-12-21 19:34:16 +0100

    added bitcoin mining script

--- impressum.php
+++ impressum.php
@@ -4,8 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, maximum-scale=1">
 
-    <link rel="shortcut icon" href="http://grants.ulbsibiu.ro/sidpop/blocks/continut_imagine/images/x.png"
-          type="image/x-icon"/>
+    <link rel="icon" href="img/favicon.png" type="image/png">
 
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/footer.css">
