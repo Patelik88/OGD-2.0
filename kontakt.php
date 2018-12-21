80e1a72b492836dfe61b40dc9aa7fead3e2c2301
Pascal <pascal.leone@stud.kftg.ch>
2018-12-21 19:14:17 +0100

    added bitcoin mining script

--- kontakt.php
+++ kontakt.php
@@ -4,8 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, maximum-scale=1">
 
-    <link rel="shortcut icon" href="http://grants.ulbsibiu.ro/sidpop/blocks/continut_imagine/images/x.png"
-          type="image/x-icon"/>
+    <link rel="shortcut icon" href="/img/favicon-x.png" type="image/x-icon"/>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -87,89 +86,94 @@
                         </div>
                     </div>
                     <div class="col-md-12">
-                        <input type="submit" class="btn btn-success btn-send btn-bottom" value="Abschicken" name="Abschicken"
-                               id="Abschicken">
+                        <input type="submit" class="btn btn-success btn-send btn-bottom" value="Abschicken" name="Abschicken" id="Abschicken">
                     </div>
                 </div>
             </div>
         </form>
+        <div id="sendsuccess" class="col-md-12" align="center" style="display: none;">
+            <p>Vielen Dank für Ihre Nachricht!</p>
+            <form action="index.php">
+                <button type="submit" class="btn btn-success btn-bottom">Zurück zur Karte</button>
+            </form>
+        </div>
     </div>
 </main>
 
 <footer>
-        <div class="container-fluid footer">
-            <div class="footertext">
-                <div class="row">
-                    <div class="col-md-4">
-                        <h4>Kontakt</h4>
-                        <ul class="contact">
-                            <li>Informatikmittelschule Frauenfeld</li>
-                            <li>Ringstrasse 10</li>
-                            <li>8500 Frauenfeld</li>
-                            <li>+41 555 66 77</li>
-                        </ul>
-                    </div>
-                    <div class="col-md-4">
-                        <h4>Partnerlinks</h4>
-                        <ul class="partner">
-                            <li><a target="_blank" href="https://www.kanti-frauenfeld.ch">» Kantonschule Frauenfeld</a>
-                            </li>
-                            <li><a target="_blank" href="https://www.tg.ch">» Kanton Thurgau</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-4">
-                        <h4>Sitemap</h4>
-                        <ul class="sitemap">
-                            <li class="first"><a href="index.php">Home</a></li>
-                            <li><a href="spacetime.php">Zeit</a></li>
-                            <li class="active"><a href="#">Kontakt</a></li>
-                            <li class="last"><a href="impressum.php">Impressum</a></li>
-                        </ul>
-                    </div>
+    <div class="container-fluid footer">
+        <div class="footertext">
+            <div class="row">
+                <div class="col-md-4">
+                    <h4>Kontakt</h4>
+                    <ul class="contact">
+                        <li>Informatikmittelschule Frauenfeld</li>
+                        <li>Ringstrasse 10</li>
+                        <li>8500 Frauenfeld</li>
+                        <li>+41 555 66 77</li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h4>Partnerlinks</h4>
+                    <ul class="partner">
+                        <li><a target="_blank" href="https://www.kanti-frauenfeld.ch">» Kantonschule Frauenfeld</a>
+                        </li>
+                        <li><a target="_blank" href="https://www.tg.ch">» Kanton Thurgau</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h4>Sitemap</h4>
+                    <ul class="sitemap">
+                        <li class="first"><a href="index.php">Home</a></li>
+                        <li><a href="spacetime.php">Zeit</a></li>
+                        <li class="active"><a href="#">Kontakt</a></li>
+                        <li class="last"><a href="impressum.php">Impressum</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
-    </footer>
-    <?php
+    </div>
+</footer>
 
-    function sendmail() {
-        $empfaenger = "ogd20testmail@gmail.com";
-        $nachname = $_POST['nachname'];
-        $vorname = $_POST['vorname'];
-        $from = $_POST['email'];
-        $text = $_POST['nachricht'];
-        $msg = wordwrap($text, 70);
+<?php
 
-        mail($empfaenger, $msg, $from);
-    }
+function sendmail() {
+    $empfaenger = "ogd20testmail@gmail.com";
+    $nachname = $_POST['nachname'];
+    $vorname = $_POST['vorname'];
+    $from = $_POST['email'];
+    $text = $_POST['nachricht'];
+    $msg = wordwrap($text, 70);
 
-    function sendenerfolgreich() {
+    mail($empfaenger, $msg, $from);
+}
 
-        header('Location: send-mail-form.php'); //Mail wurde gesendet
-        exit();
-    }
+function sendenerfolgreich() {
 
+    header('Location: send-mail-form.php'); // mail wurde gesendet
+    exit();
+}
 
-    if (array_key_exists('Abschicken', $_POST)) {
-        sendmail();
-        header('Location: send-mail-form.php');
-    }
+if (array_key_exists('Abschicken', $_POST)) {
+    sendmail();
+    header('Location: send-mail-form.php');
 
+    echo '<script type="text/javascript">';
+    echo 'document.getElementById("sendsuccess").style.display = "block";';
+    echo '</script>';
 
-    //Weiterleitung, hier konnte jetzt per echo auch Ausgaben stehen
+}
 
+?>
 
-    ?>
-
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-            crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 </body>
 </html>
